07a01dc54492c831c36778b2ef214d211852b6ff
GumpFeng <houxfeng@outloock.com>
2019-07-18 22:46:41 +0800

    资讯采集系统上传

---
+++ news-crawl/src/main/java/com/ertu/news/NewsCrawlApplication.java
@@ -0,0 +1,17 @@
+package com.ertu.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hxf
+ * @date 2019-07-18 13点57分
+ */
+@SpringBootApplication
+public class NewsCrawlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsCrawlApplication.class, args);
+    }
+
+}
